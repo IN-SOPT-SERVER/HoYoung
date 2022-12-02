9c70f2e6a3b7291df189887ef123235e63df9d12
HoYoungChun <ho723@naver.com>
2022-12-02 23:40:06 +0900

    Add seminar7 files

---
+++ seminar4/src/router/imageRouter.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import upload from "../middlewares/upload";
+import { imageController } from "../controller";
+const router: Router = Router();
+
+router.post("/", upload.single("file"), imageController.uploadImage);
+
+export default router;
--- seminar4/src/router/userRouter.ts
+++ seminar4/src/router/userRouter.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { userController } from "../controller";
+import { auth } from "../middlewares";
 
 const router: Router = Router();
 
-router.get("/:userId", userController.getUserById);
+//* 이름으로 유저 검색 - GET api/user/search?keyword=&option=
+router.get("/search", userController.searchUserByName);
+
+router.get("/:userId", auth, userController.getUserById);
 
 //* 유저 생성 - POST api/user
-router.post("/", userController.createUser);
+//router.post("/", userController.createUser);
 
 //* 전체 유저 조회 - GET api/user
 router.get("/", userController.getAllUser);
@@ -16,5 +21,28 @@ router.patch("/:userId", userController.updateUser);
 
 //* 유저 정보 삭제 - DELETE api/user/:userId
 router.delete("/:userId", userController.deleteUser);
+
+//* 회원가입 - POST api/user
+router.post(
+  "/",
+  [
+    body("name").notEmpty(),
+    body("email").notEmpty(),
+    body("password").isLength({ min: 6 }),
+  ],
+  userController.createUser
+);
+
+//* 로그인 - POST api/user/signin
+router.post(
+  "/signin",
+  [
+    body("email").notEmpty(),
+    body("email").isEmail(),
+    body("password").notEmpty(),
+    body("password").isLength({ min: 6 }),
+  ],
+  userController.signInUser
+);
 
 export default router;
